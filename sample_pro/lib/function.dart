1993a1343db6c3d4027b790f2738bc22421fa5e9
100450 <100450@MACTECH.LOC>
2024-07-17 11:29:39 +0530

    commit

---
+++ sample_pro/lib/function.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:sample_pro/studentmodel.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> storingDatatoPreff(bool isuserloggedin) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool("isloggedin", isuserloggedin);
+}
+
+Future<bool> gettingBoolData() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool? data = prefs.getBool("isloggedin");
+  if(data == null){
+    data = false;
+  }
+
+  return data;
+}
+
+void deletingString() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove("isloggedin");
+}
+
+Future <void> addStudent(StudentModel studentModel) async {
+  final  prefs = await SharedPreferences.getInstance();
+  String jsonString =  jsonEncode(studentModel.toJson());
+  print(jsonString);
+  await prefs.setString("StudentData", jsonString);
+
+}
+
+
+Future<StudentModel?> getStudent() async{
+    final prefs = await SharedPreferences.getInstance();
+    String? jsonString = prefs.getString("StudentData");
+    if (jsonString != null) {
+      Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+      return StudentModel.fromJson(jsonMap);
+    }
+     return null;
+}
---
+++ sample_pro/lib/splashscreen.dart
@@ -0,0 +1,67 @@
+// import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
+
+import 'package:flutter/material.dart';
+import 'package:sample_pro/function.dart';
+import 'package:sample_pro/linkedinlogin.dart';
+import 'package:sample_pro/profilemenu.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+   
+  @override
+  void initState(){
+     super.initState();
+     moveToNext();
+     
+  }
+
+ void moveToNext() async{
+    await Future.delayed(Duration(seconds: 2));
+    final bool isloggedin = await gettingBoolData();
+    if(isloggedin)
+    {
+   Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileScreen(),));
+ 
+    }
+    else{
+   Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(),));
+ 
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+       
+            children: [
+              Image.asset(
+                "asset/logo.jpg",
+                width: 150,
+                height: 70,
+              ),
+            ],
+          ),
+          
+        ]
+     ),
+     
+    );
+  }
+
+  
+
+}
